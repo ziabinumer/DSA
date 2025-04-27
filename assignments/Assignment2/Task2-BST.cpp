e49cb5d6fe3ada93ed9c731e4e582c7b50879ead
= <ziabinumer@gmail.com>
2025-04-27 20:10:05 +0500

    initial push

---
+++ assignments/Assignment2/Task2-BST.cpp
@@ -0,0 +1,7 @@
+/*
+    Name: Zia ur Rehman
+    Reg No: 5005-FOC/BSCS/F23 - B
+    Course: DSA (Sir. Syed Saqlain)
+
+    Code available at: https://github.com/ziabinumer/DSA/blob/main/assignments/Assignment2/Task2-BST.cpp
+*/
